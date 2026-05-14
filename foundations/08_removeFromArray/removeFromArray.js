2cbf6827eb543e63e932bdb65aab2ea5e89550f3
Asfand <yar.asfand.off@gmail.com>
2026-05-14 10:57:05 +0200

    learned about rest parameters

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    return array.filter((item) => !args.includes(item))
+    
 };
 
 // Do not edit below this line
