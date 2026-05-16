245d2f904e48202a51bd0e7819c5bafbbb345bd8
Asfand <yar.asfand.off@gmail.com>
2026-05-16 11:47:58 +0200

    Finish sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if ((Number.isInteger(a) && a > 0) && (Number.isInteger(b) && b > 0)) {
+        let sum = 0;
+        let min = a < b ? a : b;
+    let max = a > b ? a : b;
+        
+        for (let i = min; i <= max; i++){
+            sum += i
+        }
+
+        return sum
+        
+    } else {
+        return "ERROR"
+    }
 
 };
 
