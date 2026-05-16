dcdcfe7eb7b5ca8741aef4d4dd9d836ad9b18a80
Asfand <yar.asfand.off@gmail.com>
2026-05-16 12:23:30 +0200

    done Math

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (grad) {
+  return Math.round(((grad - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (grad) {
+  return Math.round(((grad * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
