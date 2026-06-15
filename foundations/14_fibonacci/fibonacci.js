d91e51bf07448590e64ac5c1097829ce6f6dff2f
Asfand <yar.asfand.off@gmail.com>
2026-06-15 21:26:58 +0200

    fun

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    index = Number(index)
+    console.log(typeof(index))
+    if (index === 0)
+        return 0
+    if (index < 0)
+        return "OOPS"
+    let fibonacciSequence = [1, 1]
+    for (let i = 2; i < index; i++){
+        fibonacciSequence.push(fibonacciSequence[i-1] + fibonacciSequence[i-2])
+    }
+    console.log(fibonacciSequence)
+    return(fibonacciSequence[index - 1])
 };
+
+console.log(fibonacci(4))
 
 // Do not edit below this line
 module.exports = fibonacci;
