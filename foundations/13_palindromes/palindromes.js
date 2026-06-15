56a2ee599499e079721d666145e1d246af3036c0
Asfand <yar.asfand.off@gmail.com>
2026-06-15 20:36:58 +0200

    difficult

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const allowedChars = "abcdefghijklmnopqrstuvwxyzäöüß123456789"
+    
+    const cleanedString = str
+        .toLowerCase()
+        .split("")
+        .filter((character) => allowedChars.includes(character))
+        .join("")
+    
+    const reversedString = cleanedString
+        .split("")
+        .reverse()
+        .join("")
+    
 
+    return cleanedString === reversedString
 };
-
+// console.log(palindromes("A car, a man, a maraca."))
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
