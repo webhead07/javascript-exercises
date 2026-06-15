c4a1a7732ff03f4033335a9408c0b7b19f71dbf2
Asfand <yar.asfand.off@gmail.com>
2026-06-15 20:37:38 +0200

    fun

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return (arr.reduce((sum, current) => sum + current,0))
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+ return (arr.reduce((current, next)=> current * next))
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a**b)
 };
 
-const factorial = function() {
-	
-};
+const factorial = function (a) {
+  let fact = 1
+    if (a === 0)
+    {
+      return fact
+    } else {
+      do {
+        fact = fact * a
+        a--
+      } while (a >= 1)
+      return fact
+    }
+  };
 
 // Do not edit below this line
 module.exports = {
