e1b89d2af50e5cbdf6d771d9b30f34440633517d
Asfand <yar.asfand.off@gmail.com>
2026-06-15 21:45:08 +0200

    first problem where i knew what to check and what method to use

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+   return (books.map((book) => book.title))
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+// const books = [
+//       {
+//         title: 'Book',
+//         author: 'Name'
+//       },
+//       {
+//         title: 'Book2',
+//         author: 'Name2'
+//       }
+//     ]
+// const getTheTitles = function (books) {
+//     console.table(books) // to see if the array of objects was coming through
+//     console.table(books.map((book) => book.title)) // map to get a new array that only outputs my desired data i.e book titles
+// };
+// getTheTitles(books)
