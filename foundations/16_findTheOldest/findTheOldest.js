03ce7894dc4d04f28521619f231c4dce2d9ea187
Asfand <yar.asfand.off@gmail.com>
2026-06-15 23:19:38 +0200

    difficult

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,56 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    people.map((person) => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear()
+        }
+    })
 
-};
-
+    const oldest = people.reduce((oldestPerson, currentPerson) => {
+        if ((oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) > (currentPerson.yearOfDeath - currentPerson.yearOfBirth)) {
+            return oldestPerson
+        }
+        return currentPerson
+    })
+    return oldest
+    }
 // Do not edit below this line
 module.exports = findTheOldest;
+
+//--------------------------------
+
+//    const people = [
+//       {
+//         name: "Carly",
+//         yearOfBirth: 1066,
+//       },
+//       {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//       },
+//       {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//       },
+//     ]
+
+// const findTheOldest = function (people) {
+//     people.map((person) => {
+//         if (!person.yearOfDeath) {
+//             person.yearOfDeath = new Date().getFullYear()
+//         }
+//      return person
+//     })
+
+//     const oldest = people.reduce((oldestPerson, currentPerson) => {
+//         if ((oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) > (currentPerson.yearOfDeath - currentPerson.yearOfBirth)) {
+//             return oldestPerson
+//         }
+//         return currentPerson
+//     })
+//     return oldest
+//     }
+
+
+// console.log(findTheOldest(people))
